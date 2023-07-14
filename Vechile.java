31473b59db60b0ae3130962d62e89c97de2c3b39
kishan <108215645+kishan9182736@users.noreply.github.com>
2023-07-14 11:25:45 +0600

    Add files via upload

---
+++ Vechile.java
@@ -0,0 +1,11 @@
+public class Vechile {
+ double speed ;
+ void go(){
+     System.out.println("This vechile is runnig ");
+ }
+ void stop(){
+     System.out.println("This vechile is stopped");
+ }
+
+
+}
